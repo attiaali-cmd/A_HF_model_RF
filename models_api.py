b3919b9948f2b40b949490eeaba39d6819881442
attiaali-cmd <attia.ali@pharma.cu.edu.eg>
2026-03-22 02:58:07 +0200

    Update models_api.py

--- models_api.py
+++ models_api.py
@@ -1,51 +1,27 @@
 import pickle
-import streamlit as st
-from streamlit_option_menu import option_menu
-import pandas as pd 
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-# Load models
-random_forest = pickle.load(open("randomforest.sav", 'rb'))
-decision_tree = pickle.load(open("decisiontree.sav", 'rb'))
+app = FastAPI()
 
-# Sidebar menu
+class ModelInput(BaseModel):
+    serum_creatinine: float
+    ejection_fraction: int
+    age: int
 
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Select The Model",
-        options=["RandomForest model", "DecisionTree Model"],
-        icons=["activity", "diagram-3"],  # optional
-        default_index=0
-    )
+model = pickle.load(open("randomforest.sav", "rb"))
 
-st.title('Heart Failure Survival Prediction')
+@app.post("/Heart_failure_survival")
+def predict(data: ModelInput):
+    input_list = [
+        data.serum_creatinine,
+        data.ejection_fraction,
+        data.age
+    ]
 
-if selected == "RandomForest model":
-    st.header("Random Forest Model")
-    serum_creatinine=st.text_input('What is patient Serum Creatinine?')
-    ejection_fraction=st.text_input('What is patient Ejection Fraction?')
-    age=st.text_input('What is the patient Age?')
-    hf_survival=''
-    if st.button('Heart Failure Survival'):
-        hf_predict=random_forest.predict([[serum_creatinine,ejection_fraction,age]])
-        if (hf_predict[0]==1):
-            hf_survival='Will Not Survive'
-        else:
-            hf_survival='Will Survive'
-    st.success(hf_survival)
+    prediction = model.predict([input_list])
 
-elif selected == "DecisionTree Model":
-    st.header("Decision Tree Model")
-
-    serum_creatinine=st.text_input('What is patient Serum Creatinine?')
-    ejection_fraction=st.text_input('What is patient Ejection Fraction?')
-    age=st.text_input('What is the patient Age?')
-    hf_survival=''
-    if st.button('Heart Failure Survival'):
-        hf_predict=random_forest.predict([[serum_creatinine,ejection_fraction,age]])
-        if (hf_predict[0]==1):
-            hf_survival='Will Not Survive'
-        else:
-            hf_survival='Will Survive'
-    st.success(hf_survival)
-
-    
+    if prediction[0] == 1:
+        return {"result": "Will Survive"}
+    else:
+        return {"result": "Will Not Survive"}
